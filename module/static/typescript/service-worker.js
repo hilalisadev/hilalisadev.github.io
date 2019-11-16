5d50e2d3bc2a0894ce381b0e671aa3f5d83e45d4
HILALISA <hilalisadev@outlook.fr>
2019-11-17 00:29:50 +0100

    add typescript

---
+++ module/static/typescript/service-worker.js
@@ -0,0 +1,35 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "precache-manifest.a122b2567c9493eb570019a9a840a929.js"
+);
+
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("index.html");
+
+workbox.routing.registerRoute(/\/@webcomponents\/webcomponentsjs\//, workbox.strategies.staleWhileRevalidate(), 'GET');
+workbox.routing.registerRoute(/^https:\/\/fonts.gstatic.com\//, workbox.strategies.staleWhileRevalidate(), 'GET');
