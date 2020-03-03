73f995c8a7358b9eefb9a313852060268fcc5137
HILALISA <hilalisadev@outlook.fr>
2020-03-03 09:31:54 +0100

    add typescript

---
+++ module/static/typescript/precache-manifest.8ac9d22bbd22d60e79b0f8a0b63d7768.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "09885b54b4713eb89c1c7cf62db1219d",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "16f814bcba6174deaf52",
+    "url": "main.js"
+  },
+  {
+    "revision": "26357f582f7a0386c12e79b38a3c6f13",
+    "url": "index.html"
+  },
+  {
+    "revision": "59f6d3942d80d46de33e",
+    "url": "5.js"
+  },
+  {
+    "revision": "64384935186941f0e99d",
+    "url": "4.js"
+  },
+  {
+    "revision": "f8d051b56b483fdc84b8",
+    "url": "3.js"
+  },
+  {
+    "revision": "8185318b9a2542fa8162",
+    "url": "2.js"
+  },
+  {
+    "revision": "c81d09cc4ac841d38c2d",
+    "url": "1.js"
+  }
+];
